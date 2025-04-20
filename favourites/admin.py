64dffcbc905b204bf8b7f10a5bb81cf9d6dc6c7a
Bagyshan <Bagishan01@gmail.com>
2025-04-21 04:24:54 +0600

    favourites admin

--- favourites/admin.py
+++ favourites/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Favorite
 
-# Register your models here.
+@admin.register(Favorite)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', )
