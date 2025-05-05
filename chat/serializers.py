35779da443d7a53a1dfdc5698362b0ee0a9aced2
Bagyshan <Bagishan01@gmail.com>
2025-05-05 17:54:29 +0600

    hot fix

--- chat/serializers.py
+++ chat/serializers.py
@@ -21,23 +21,66 @@ class UserSerializer(serializers.ModelSerializer):
         return request.build_absolute_uri(obj.avatar.url)
 
 
+# class ChatSerializer(serializers.ModelSerializer):
+#     participants = serializers.PrimaryKeyRelatedField(
+#         queryset=User.objects.all(), many=True, required=False
+#     )
+
+#     class Meta:
+#         model = Chat
+#         fields = ['id', 'participants', 'created_at']
+
+#     # def create(self, validated_data):
+#     #     request = self.context['request']
+#     #     chat = Chat.objects.create()
+#     #     chat.participants.add(request.user)
+#     #     other_users = validated_data.get('participants', [])
+#     #     for user in other_users:
+#     #         chat.participants.add(user)
+#     #     return chat
+
+#     def create(self, validated_data):
+#         request_user = self.context['request'].user
+#         other_users = validated_data.get('participants', [])
+
+#         if len(other_users) != 1:
+#             raise serializers.ValidationError("Chat must be between exactly two users.")
+
+#         other_user = other_users[0]
+#         desired_ids = {request_user.id, other_user.id}
+
+#         # Получаем все чаты с двумя участниками
+#         existing_chats = Chat.objects.annotate(
+#             num_participants=models.Count('participants')
+#         ).filter(num_participants=2)
+
+#         # Перебираем и сравниваем точный набор участников
+#         for chat in existing_chats:
+#             chat_user_ids = set(chat.participants.values_list('id', flat=True))
+#             if chat_user_ids == desired_ids:
+#                 return chat
+
+#         # Если не найден — создаем новый
+#         chat = Chat.objects.create()
+#         chat.participants.add(request_user, other_user)
+#         return chat
+
+#     def to_representation(self, instance):
+#         rep = super().to_representation(instance)
+#         rep['participants'] = UserSerializer(instance.participants.all(), many=True, context=self.context).data
+#         return rep
+    
 class ChatSerializer(serializers.ModelSerializer):
-    participants = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all(), many=True, required=False
-    )
+    participants = serializers.SerializerMethodField()
 
     class Meta:
         model = Chat
         fields = ['id', 'participants', 'created_at']
 
-    # def create(self, validated_data):
-    #     request = self.context['request']
-    #     chat = Chat.objects.create()
-    #     chat.participants.add(request.user)
-    #     other_users = validated_data.get('participants', [])
-    #     for user in other_users:
-    #         chat.participants.add(user)
-    #     return chat
+    def get_participants(self, obj):
+        request_user = self.context['request'].user
+        other_users = obj.participants.exclude(id=request_user.id)
+        return UserSerializer(other_users, many=True, context=self.context).data
 
     def create(self, validated_data):
         request_user = self.context['request'].user
@@ -49,47 +92,53 @@ class ChatSerializer(serializers.ModelSerializer):
         other_user = other_users[0]
         desired_ids = {request_user.id, other_user.id}
 
-        # Получаем все чаты с двумя участниками
         existing_chats = Chat.objects.annotate(
             num_participants=models.Count('participants')
         ).filter(num_participants=2)
 
-        # Перебираем и сравниваем точный набор участников
         for chat in existing_chats:
             chat_user_ids = set(chat.participants.values_list('id', flat=True))
             if chat_user_ids == desired_ids:
                 return chat
 
-        # Если не найден — создаем новый
         chat = Chat.objects.create()
         chat.participants.add(request_user, other_user)
         return chat
 
-    def to_representation(self, instance):
-        rep = super().to_representation(instance)
-        rep['participants'] = UserSerializer(instance.participants.all(), many=True, context=self.context).data
-        return rep
-    
-
 class MessageByUserSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(source='sender.name')
+    name = serializers.SerializerMethodField()
 
     class Meta:
         model = Message
         fields = ['id', 'name', 'created_at', 'content']
 
+    def get_name(self, obj):
+        request = self.context.get('request')
+        if request and obj.sender == request.user:
+            return "me"
+        return obj.sender.name
+
 class ParticipantWithMessagesSerializer(serializers.ModelSerializer):
     message = serializers.SerializerMethodField()
     avatar = serializers.SerializerMethodField()
+    name = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ['id', 'name', 'avatar', 'message']
+        fields = ['id', 'name', 'avatar', 'is_company', 'last_login', 'message']
+
+    def get_name(self, obj):
+        request = self.context.get('request')
+        if request and obj == request.user:
+            return "me"
+        return obj.name
 
     def get_message(self, user):
         chat = self.context.get('chat')
+        request = self.context.get('request')
         messages = chat.messages.filter(sender=user)
-        return MessageByUserSerializer(messages, many=True).data
+        return MessageByUserSerializer(messages, many=True, context={'request': request}).data
+
     
     def get_avatar(self, obj):
         request = self.context.get('request')
